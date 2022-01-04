d507b9101d9c2294d26f8aa61be851977280a7c1
JefersonEGomes <jefersonsportsempre@gmail.com>
2022-01-04 16:43:02 -0300

    Atualizando os arquivos

--- exercicios/src/controle/DesafioWhile.java
+++ exercicios/src/controle/DesafioWhile.java
@@ -6,39 +6,44 @@ public class DesafioWhile {
 	public static void main(String[] args) {
 
 		/*
-		 
-		 		To/DO 
-		 1 - Criar scanner V 
-		 2 - Criar Variaveis do programa V 
-		 3 - Criar while para verificar a nota e adicionar valor as variaveis V 
-		 4 - registar a soma das notas digitadas V
-		 5 - registrar quantas notas foram digitas V
-		 6 - calular a média V
-		 7 - sair do programa se o usuario digitar -1 V
-		  
-		 */
+	  
+	 				-----------------To/DO----------------- 
+	 1 - Criar scanner V 
+	 2 - Criar Variaveis do programa V 
+	 3 - Criar while para verificar a nota e adicionar valor as variaveis V 
+	 4 - registar a soma das notas digitadas V 
+	 5 - registrar quantas notas foram digitas V 
+	 6 - calular a média V 
+	 7 - sair do programa se o usuario digitar -1 V
+	  
+	 */
 
-		Scanner leitor = new Scanner(System.in);
-		double nota = 0;
-		int count = 0;
-		double soma = 0;
-		double media = 0;
+	Scanner leitor = new Scanner(System.in);
+	double nota = 0;
+	double contador = 0;
+	double soma = 0;
+	double media = 0;
+	
+	while (nota != -1) {
+		System.out.println("Por favor digite uma nota válida abaixo: ");
+		nota = leitor.nextDouble();
 
-		while (nota != -1) {
-			System.out.println("Por favor digite uma nota válida abaixo: ");
-			nota = leitor.nextInt();
+		if (nota >= 0 && nota <= 10) {
+			contador++;
+			soma = soma + nota;
 
-			if (nota >= 0 && nota <= 10) {
-				count++;
-				soma = soma + nota;
+		} else if (nota < -1 && nota < 0 || nota > 10) {
 
-			} else if (nota < -1 && nota < 0 || nota > 10 ) {
+			System.out.println("Favor digitar uma nota válida");
 
-				System.out.println("Favor digitar uma nota válida");
+		} else if (nota == -1 && contador == 0) {
+			System.out.println("Aqui está sua média: 0");
+		}
+		
+		else {
 
-			} else {
-				media = soma / count;
-				System.out.println("Aqui está sua média: " + media);
+			media = soma / contador;
+			System.out.println("Aqui está sua média: " + media);
 			}
 		}
 
